0f6f7304f00246e62bca2855844e6f5499b4138c
Roberto Carlos Franco Tenorio <robertofrancotenorio2@gmail.com>
2017-09-08 10:49:19 -0500

    Crear clase Figura

---
+++ Figura.java
@@ -0,0 +1,32 @@
+/**
+ * Esta clase es un super clase para representar las
+ * caracteristicas comunes de muchas figuras
+ * 
+ * @Autor: Roberto Franco
+ * @version 1.0
+ */
+public class Figura
+{
+    protected float area;
+    
+    /**
+     * Constructor de la clase figura
+     */
+    public Figura()
+    {
+        area=0;
+    }
+    
+    /**
+     * Calcula el area de la figura
+     */
+    public void calculaArea()
+    {
+        System.out.println("No se como calcular el area");
+    }
+    
+    public void imprimirArea()
+    {
+        System.out.println("El area de la figrua es: "+area);
+    }
+}
